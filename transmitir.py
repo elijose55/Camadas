3aab03c7b0049ccbf179aa21ff287b39e3e485f5
elijose55 <elijose55@hotmail.com>
2018-08-23 14:00:03 -0300

    adicionando etapa2

---
+++ transmitir.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+#Carareto
+#17/02/2018
+#  Aplicação
+####################################################
+
+print("comecou")
+
+from enlace import *
+import time
+import cv2
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import time
+
+# voce deverá descomentar e configurar a porta com através da qual ira fazer a
+# comunicaçao
+# Serial Com Port
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+serialName = "COM3"                  # Windows(variacao de)
+
+
+
+print("porta COM aberta com sucesso")
+
+
+
+def main():
+    # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
+    com = enlace(serialName)
+
+    # Ativa comunicacao
+    com.enable()
+
+    #verificar que a comunicação foi aberta
+    print("comunicação aberta")
+
+###################################################### transmissao
+    # a seguir ha um exemplo de dados sendo carregado para transmissao
+    # voce pode criar o seu carregando os dados de uma imagem. Tente descobrir
+    #como fazer isso
+    print ("gerando dados para transmissao :")
+
+
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+
+    txBuffer = open(filename, "rb").read()
+    txLen = len(txBuffer)
+    print("Tempo estimado: {}" .format((txLen*10)/115200))
+    print("tentado transmitir .... {} bytes".format(txLen))
+    start = time.time()
+    #txBuffer = bytearray(txBuffer)
+    
+
+
+
+
+    
+
+
+    # Transmite dado
+    print("tentado transmitir .... {} bytes".format(txLen))
+    
+    com.sendData(txBuffer)
+
+        
+    # Atualiza dados da transmissão
+    txSize = com.tx.getStatus()
+   
+    end = time.time()
+    
+    # Encerra comunicação
+    print("-------------------------")
+    print("Comunicação encerrada")
+    print("-------------------------")
+    com.disable()
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
